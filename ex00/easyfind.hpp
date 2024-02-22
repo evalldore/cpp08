c832bafc2100d1c0549f2cc24f139942e836742c
evalldore <evalldore@gmail.com>
2024-02-22 01:42:59 -0500

    first commit

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+template<typename T>
+int easyfind(T& var, int num) {
+	if (std::find(var.begin(), var.end(), num) != var.end()) {
+		std::cout << "Found " << num << " in container!" << std::endl;
+		return 1;
+	}
+	std::cout << "Missing " << num << " in container!" << std::endl;
+	return 0;
+}
---
+++ ex00/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include "easyfind.hpp"
+
+int main(void) {
+	std::vector<int> vec;
+
+	for (int i = 0; i < 10; i++) {
+		vec.push_back(i);
+	}
+	easyfind(vec, -3);
+}
