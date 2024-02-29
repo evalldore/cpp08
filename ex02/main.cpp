b6c1665a1d76a7fa43a39b46824a9dfe27f18224
evalldore <evalldore@gmail.com>
2024-02-29 00:10:20 -0500

    list test outputs

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <list>
 #include "MutantStack.hpp"
 
+#define RESET "\e[0m"
+#define BLK "\e[0;30m"
+#define RED "\e[0;31m"
+#define GRN "\e[0;32m"
+#define YEL "\e[0;33m"
+#define BLU "\e[0;34m"
+#define MAG "\e[0;35m"
+#define CYN "\e[0;36m"
+#define WHT "\e[0;37m"
+
+
 static void subject(void) {
+	std::cout << YEL << "Mutant stack subject tests" << RESET << std::endl;
 	MutantStack<int> mstack;
 	mstack.push(5);
 	mstack.push(17);
@@ -23,6 +36,27 @@ static void subject(void) {
 		++it;
 	}
 	std::stack<int> s(mstack);
+
+	std::cout << YEL << "List subject tests" << RESET << std::endl;
+	std::list<int> list;
+	list.push_back(5);
+	list.push_back(17);
+	std::cout << list.back() << std::endl;
+	list.pop_back();
+	std::cout << list.size() << std::endl;
+	list.push_back(3);
+	list.push_back(5);
+	list.push_back(737);
+	list.push_back(0);
+	std::list<int>::iterator lit = list.begin();
+	std::list<int>::iterator lite = list.end();
+	++lit;
+	--lit;
+	while (lit != lite)
+	{
+		std::cout << *lit << std::endl;
+		++lit;
+	}
 }
 
 int main(void) {
